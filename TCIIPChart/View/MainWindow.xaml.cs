e267ce7a7e813083eba105d9723fe2f5bf7ec901
dh.cheon <34930947+Dolarge@users.noreply.github.com>
2022-01-04 18:24:42 +0900

    WPF chart

---
+++ TCIIPChart/View/MainWindow.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System.Windows;
+using TCIIPChart.ViewModel;
+
+namespace TCIIPChart
+{
+    /// <summary>
+    /// 간단한 tcp/ip로 비동기 통신 예제
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            ViewModel.SeverViewModel severViewModel = new ViewModel.SeverViewModel();
+            this.DataContext = severViewModel;
+
+            //if close window
+            if (severViewModel.CloseAction == null)
+            {
+                //socket dipose 
+
+                //close window
+                severViewModel.CloseAction = new System.Action(() => this.Close());
+
+            }
+
+        }
+
+
+
+    }
+}
---
+++ TCIIPChart/ViewModel/SeverViewModel.cs
@@ -0,0 +1,123 @@
+﻿using Prism.Commands;
+using Prism.Mvvm;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using System.Windows.Threading;
+using TCIIPChart.Model;
+
+namespace TCIIPChart.ViewModel
+{
+
+    public interface IViewModel { }
+
+    public class SeverViewModel : BindableBase, IViewModel
+    {
+
+        public ObservableCollection<MessageModel> ServerChart { get; set; } = new ObservableCollection<MessageModel>();
+
+        public ICommand ConnectBtnClick { get;private set;  }
+
+
+        public SeverViewModel()
+        {
+            this.ConnectBtnClick = new DelegateCommand<object>(this.connectionListen, Cansubmit);
+        }
+
+        private bool Cansubmit(object arg) { return true; }
+
+
+        public static string stringData = null;
+        
+        //Creat TCP/IP socket
+        private Socket listener = null;
+        private Socket handler = null;
+
+        
+        public static string getLocalIPAddress()
+        {
+            IPHostEntry host;
+            string localIP = "";
+            host = Dns.GetHostEntry(Dns.GetHostName());
+
+            foreach (IPAddress ip in host.AddressList)
+            {
+                if (ip.AddressFamily == AddressFamily.InterNetwork)
+                {
+                    localIP = ip.ToString();
+                    return localIP;
+                }
+            }
+
+            return "127.0.0.1";
+            
+        }
+
+        public void connectionListen(object sender)
+        {
+            Console.WriteLine("서버콘솔창 \n\n\n");
+            ServerChart.Add(new MessageModel { 
+                Time= DateTime.Now,
+                message="TEST"});
+
+            // TcpListener 생성자에 붙는 매개변수는 
+            // 첫번째는 IP를 두번째는 port 번호입니다.
+            TcpListener server = new TcpListener(IPAddress.Any, 9999);
+
+            // 서버를 시작합니다.
+            server.Start();
+
+
+            // 클라이언트 객체를 만들어 9999에 연결한 client를 받아옵니다
+            // 클라이언트가 접속할때까지 서버는 해당구문에서 블락됩니다.
+            TcpClient client = server.AcceptTcpClient();
+
+
+            // NetworkStream 객체를 만들어 client에서 보낸 데이터를 받을 객체를 생성합니다.
+            NetworkStream ns = client.GetStream();
+
+            // Socket은 byte[] 형식으로 데이터를 주고받으므로 byte[]형 변수를 선언합니다.
+            byte[] byteData = new byte[1024];
+
+            // 클라이언트가 write한 데이터를 읽어옵니다.
+            // 아래의 작업 이후에 byteData에는 읽어온 데이터가 들어갑니다.
+            // 동기서버의 경우 해당코드에서 읽을데이터가 올때까지 대기합니다.
+            ns.Read(byteData, 0, byteData.Length);
+
+
+            // 출력을 위해 byteData를 string형으로 바꿔줍니다.
+            string stringData = Encoding.Default.GetString(byteData);
+
+            Console.WriteLine(stringData);
+
+            server.Stop();
+            ns.Close();
+
+        }
+        
+        public Action CloseAction { get; set; }
+
+        public void CloseSocket()
+        {
+            if (handler != null)
+            {
+                handler.Close();
+                handler.Dispose();
+            }
+            if (listener!=null)
+            {
+                listener.Close();
+                listener.Dispose();
+            }
+
+        }
+    }
+}
