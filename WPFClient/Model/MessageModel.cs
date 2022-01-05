ad6c2149c70941118c3429810857eaaaa5bfc9c8
dh.cheon <34930947+Dolarge@users.noreply.github.com>
2022-01-05 13:40:23 +0900

    feature : simple connect

---
+++ WPFClient/Model/MessageModel.cs
@@ -0,0 +1,28 @@
+﻿using Prism.Mvvm;
+using System;
+
+namespace WPFClient.Model
+{
+    public class MessageModel :BindableBase
+    {
+        private string _message;
+        public string message 
+        {
+            get { return _message; }
+            set 
+            {
+                SetProperty(ref _message, value); 
+            }  
+        }
+        private DateTime _time;
+        public DateTime Time
+        {
+            get { return _time; }
+            set
+            {
+                SetProperty(ref _time, value);
+            }
+        }
+
+    }
+}
---
+++ WPFClient/View/MainWindow.xaml.cs
@@ -0,0 +1,13 @@
+﻿using System.Windows;
+
+namespace WPFClient
+{
+
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
