75f87b85fca5f40880ae0de8615e466d14bb35cc
dh.cheon <34930947+Dolarge@users.noreply.github.com>
2022-01-05 09:19:51 +0900

    feature : add TCPListener

--- TCIIPChart/obj/Debug/net5.0-windows7.0/App.g.i.cs
+++ TCIIPChart/obj/Debug/net5.0-windows7.0/App.g.i.cs
@@ -45,7 +45,7 @@ namespace TCIIPChart {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.0.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "5.0.10.0")]
         public void InitializeComponent() {
             
             #line 5 "..\..\..\App.xaml"
@@ -60,7 +60,7 @@ namespace TCIIPChart {
         /// </summary>
         [System.STAThreadAttribute()]
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.0.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "5.0.10.0")]
         public static void Main() {
             TCIIPChart.App app = new TCIIPChart.App();
             app.InitializeComponent();
