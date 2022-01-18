c3a4b26ceda8f4b38b6d7720be5a26154f1b23d2
dh.cheon <dohyun722@daum.net>
2022-01-18 18:09:46 +0900

    feature  : coderefactoring

--- TCIIPChart/ViewModel/SeverViewModel.cs
+++ TCIIPChart/ViewModel/SeverViewModel.cs
@@ -1,17 +1,11 @@
 ﻿using Prism.Commands;
 using Prism.Mvvm;
 using System;
-using System.Collections.Generic;
 using System.Collections.ObjectModel;
-using System.ComponentModel;
-using System.Linq;
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
-using System.Threading;
-using System.Threading.Tasks;
 using System.Windows.Input;
-using System.Windows.Threading;
 using TCIIPChart.Model;
 
 namespace TCIIPChart.ViewModel
